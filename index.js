f19980981e0e9596384317c17532e478df471530
ocl5 <ocl5@alu.ua.es>
2020-10-13 16:57:05 +0200

    Prueba commit rama

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hola',  (request, response) => {
-    response.send('Hola a todas y a todos desde Express!');
+    response.send('Hola a todas y a todos desde Express');
 });
 
 app.listen(8080, () => {
