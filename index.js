e09faaa72b34abdad5c3fb16474e64d23a0b7e2d
ocl5 <ocl5@alu.ua.es>
2020-10-21 19:52:06 +0200

    Incorporamos nodemon y morgan

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict'
 
 const express = require('express');
+
+const logger = require('morgan')
+
 const app = express();
+
+
+app.use(logger('dev'));
 
 app.get('/hola',  (request, response) => {
     response.send('Hola a todas y a todos desde Express');
 });
 
-app.listen(8080, () => {
-    console .log('API REST ejecutándose en http//localhost:80/hola');
+app.listen(3000, () => {
+    console .log('API REST ejecutándose en http//localhost:3000/hola');
 });
